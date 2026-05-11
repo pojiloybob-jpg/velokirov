0d907a7c9a7963b701202bbb716961dbf091f388
pojiloybob-jpg <pojiloybob@gmail.com>
2026-05-11 13:45:39 +0300

    Add files via upload

---
+++ layers/_10.js
@@ -0,0 +1,27 @@
+var json__10 = {
+"type": "FeatureCollection",
+"name": "_10",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Стадион \"Трудовые резервы\"" }, "geometry": { "type": "Point", "coordinates": [ 49.686496301712246, 58.601381396552874 ] } },
+{ "type": "Feature", "properties": { "name": "Вятская Филармония" }, "geometry": { "type": "Point", "coordinates": [ 49.684331861539398, 58.594120432919482 ] } },
+{ "type": "Feature", "properties": { "name": "Стадион \"Динамо\"" }, "geometry": { "type": "Point", "coordinates": [ 49.685096828066229, 58.605564263106842 ] } },
+{ "type": "Feature", "properties": { "name": "Свято-Серафимовский собор" }, "geometry": { "type": "Point", "coordinates": [ 49.687753456004415, 58.596763907146553 ] } },
+{ "type": "Feature", "properties": { "name": "овраг Засора" }, "geometry": { "type": "Point", "coordinates": [ 49.682988722637589, 58.600604390980791 ] } },
+{ "type": "Feature", "properties": { "name": "Часовня Михаила Архангела" }, "geometry": { "type": "Point", "coordinates": [ 49.683196271695259, 58.607978296982232 ] } },
+{ "type": "Feature", "properties": { "name": "Магометанская мечеть" }, "geometry": { "type": "Point", "coordinates": [ 49.681986557187699, 58.608477145646276 ] } },
+{ "type": "Feature", "properties": { "name": "Парк им. Ю.А. Гагарина" }, "geometry": { "type": "Point", "coordinates": [ 49.686312472546852, 58.594458796409214 ] } },
+{ "type": "Feature", "properties": { "name": "Спасская улица" }, "geometry": { "type": "Point", "coordinates": [ 49.68273373379531, 58.60221244901215 ] } },
+{ "type": "Feature", "properties": { "name": "Собор Спаса Нерукотворного Образа" }, "geometry": { "type": "Point", "coordinates": [ 49.684749924641245, 58.602852716666341 ] } },
+{ "type": "Feature", "properties": { "name": "Александровский сад" }, "geometry": { "type": "Point", "coordinates": [ 49.684904103941214, 58.607217654900808 ] } },
+{ "type": "Feature", "properties": { "name": "Набережная Грина" }, "geometry": { "type": "Point", "coordinates": [ 49.686760185514068, 58.610989041670507 ] } },
+{ "type": "Feature", "properties": { "name": "Церковь в честь Федоровской иконы" }, "geometry": { "type": "Point", "coordinates": [ 49.685932156274475, 58.612044841043435 ] } },
+{ "type": "Feature", "properties": { "name": "Памятник Александру Невскому" }, "geometry": { "type": "Point", "coordinates": [ 49.682611801946642, 58.594037909961216 ] } },
+{ "type": "Feature", "properties": { "name": "Часовня во имя Святителя Стефана Пермского" }, "geometry": { "type": "Point", "coordinates": [ 49.683617723530695, 58.598939233369173 ] } },
+{ "type": "Feature", "properties": { "name": "Трифонов монастырь" }, "geometry": { "type": "Point", "coordinates": [ 49.69006212647318, 58.60094604782622 ] } },
+{ "type": "Feature", "properties": { "name": "Церковь Преображения Господня" }, "geometry": { "type": "Point", "coordinates": [ 49.688497488628265, 58.606009776960008 ] } },
+{ "type": "Feature", "properties": { "name": "Лондонская телефонная будка" }, "geometry": { "type": "Point", "coordinates": [ 49.691378187852727, 58.596787045034155 ] } },
+{ "type": "Feature", "properties": { "name": "Центральная ротонда" }, "geometry": { "type": "Point", "coordinates": [ 49.685292129954895, 58.608320525339423 ] } },
+{ "type": "Feature", "properties": { "name": "Береговая ротонда" }, "geometry": { "type": "Point", "coordinates": [ 49.688111265849166, 58.608841466368055 ] } }
+]
+}
